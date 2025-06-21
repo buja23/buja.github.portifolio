1c2c7683ccd6c65af8813f83e933c79eaa730122
buja23 <jooj.soares.227@gmail.com>
2025-06-21 00:41:19 -0300

    botoes

--- portifolio/src/pages/sections/About/Hero/Hero.tsx
+++ portifolio/src/pages/sections/About/Hero/Hero.tsx
@@ -1,25 +1,47 @@
-import { styled } from "@mui/material"
+import { styled, Container, Typography, Button } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png";
+import Grid from "@mui/material/Grid";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Hero = () => {
   
+    const StylesImg = styled("img")(()=> ({
+    width: "100%",
+    borderRadius: "50%",
+}))
+
     const StylesHero = styled("div")(()=> ({
         backgroundColor: "black",	
+        height: "100vh",
     }))
 
-        const StylesImg = styled("img")(()=> ({
-        width: "30%",
-        borderRadius: "50%",
-    }))
 
   return (
     <>
         <StylesHero>
-           buja
-           <StylesImg src={Avatar}/>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid size={4}>
+                        <StylesImg src={Avatar}/>
+                    </Grid>
+                    <Grid size={8}>
+                        <Typography color="primary" variant="h1">Victor Azambuja</Typography>
+                        <Typography color="primary" variant="h2">desenvolvedor de software</Typography>
+                        <Button>
+                            <FileDownloadIcon />
+                            Baixar CV
+                        </Button>
+                        <Button>
+                            <EmailIcon/>
+                            Contato
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
         </StylesHero>
-       
-        
+
+
     </>
   )
 }
