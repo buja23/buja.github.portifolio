acd52098a33cf2365e478e16599d05490bf5c1f6
buja23 <jooj.soares.227@gmail.com>
2025-10-14 03:35:47 -0300

    "commit final"

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/buja23/buja.github.portifolio/',
+  base: '/buja.github.portifolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
